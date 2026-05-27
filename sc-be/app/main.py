243d77107b5639240dd0211e09b5fd531bda32ff
asentrix510 <asentrix510@gmail.com>
2026-05-27 20:40:10 +0530

    Containerized frontend with Docker Compose

--- sc-be/app/main.py
+++ sc-be/app/main.py
@@ -24,8 +24,8 @@ app.mount(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
